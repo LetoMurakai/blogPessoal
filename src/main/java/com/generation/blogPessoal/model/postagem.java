d87bbb34e9b78dfe103ac440195911463aa3b988
Leto Murakai <letocrazy@hotmail.com>
2021-05-28 18:50:31 -0300

    Enviando atividade

--- src/main/java/com/generation/blogPessoal/model/postagem.java
+++ src/main/java/com/generation/blogPessoal/model/postagem.java
@@ -6,10 +6,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.sun.istack.NotNull;
 
 @Entity
@@ -32,6 +35,11 @@ public class postagem {
 	
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date data = new java.sql.Date(System.currentTimeMillis());
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
+	
 	
 	//getters e setters
 	public long getId() {
@@ -57,6 +65,12 @@ public class postagem {
 	}
 	public void setData(Date data) {
 		this.data = data;
+	}
+	public Tema getTema() {
+		return tema;
+	}
+	public void setTema(Tema tema) {
+		this.tema = tema;
 	}
 	
 	
